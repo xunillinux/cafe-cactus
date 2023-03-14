3ba54e96e0dd62a51815d225ba0cc2f64dfd2d35
Jonas Meise <33724765+xunillinux@users.noreply.github.com>
2023-03-14 19:23:12 +0100

    Delete .netlify directory

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const file = '_app/immutable/entry/error.svelte.89bf735a.js';
-export const imports = ["_app/immutable/entry/error.svelte.89bf735a.js","_app/immutable/chunks/index.4a373090.js","_app/immutable/chunks/singletons.3221ffd3.js","_app/immutable/chunks/index.36c6109f.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .netlify/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/_page.svelte.2151a5a5.js';
-export const imports = ["_app/immutable/entry/_page.svelte.2151a5a5.js","_app/immutable/chunks/index.4a373090.js","_app/immutable/chunks/runtime.esm.a09f9dbd.js","_app/immutable/chunks/index.36c6109f.js"];
-export const stylesheets = [];
-export const fonts = [];
